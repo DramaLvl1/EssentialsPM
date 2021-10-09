3be46e989969f894815fae05341c1a0f83a29647
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-10-09 19:30:13 +0200

    Update EssentialsPM.php

--- src/Drama_Lvl1/EssentialsPM/EssentialsPM.php
+++ src/Drama_Lvl1/EssentialsPM/EssentialsPM.php
@@ -2,14 +2,15 @@
 
 namespace Drama_Lvl1\EssentialsPM;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\Server;
 
 class Main extends PluginBase implements Listener{
     
     public function onEnable() : void {
+        $this->saveResource("config.yml");
+        Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("tell"));
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 }
