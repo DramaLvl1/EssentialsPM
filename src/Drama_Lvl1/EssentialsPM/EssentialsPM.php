2fa987b8d797b78295e61e1a1732691928add97c
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-10-10 16:41:06 +0200

    Update EssentialsPM.php

--- src/Drama_Lvl1/EssentialsPM/EssentialsPM.php
+++ src/Drama_Lvl1/EssentialsPM/EssentialsPM.php
@@ -12,5 +12,14 @@ class Main extends PluginBase implements Listener{
         $this->saveResource("config.yml");
         Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("tell"));
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $p = $cfg->get("Prefix");
+        if($cfg->get("version") === 1){
+            $this->getServer()->getLogger()->notice($p . " §aThis config is up to date");
+        } else {
+            $this->getServer()->getLogger()->alert($p . " This config is outdated.");
+            $this->getServer()->getLogger()->alert($p . " Please delete this config to recive the new config version");
+            $this->getServer()->getLogger()->alert($p . " And dont forget to copy your messages");
+        }
     }
 }
