20a1b5fc68d7c8c9fddc44479afcaa48ccc5e93f
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-10-11 11:38:56 +0200

    Update EssentialsPM.php

--- src/Drama_Lvl1/EssentialsPM/EssentialsPM.php
+++ src/Drama_Lvl1/EssentialsPM/EssentialsPM.php
@@ -10,16 +10,26 @@ class Main extends PluginBase implements Listener{
     
     public function onEnable() : void {
         $this->saveResource("config.yml");
-        Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("tell"));
+        
+        $this->getServer()->getCommandMap()->unregister($this->getServer()->getCommandMap()->getCommand("tell"));
+        $this->getServer()->getCommandMap()->register("tell", new TellCommand($this));
+        $this->getServer()->getCommandMap()->register("reply", new ReplyCommand($this));
+        
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $p = $cfg->get("Prefix");
         if($cfg->get("version") === 1){
-            $this->getServer()->getLogger()->notice($p . " §aThis config is up to date");
+            $this->getServer()->getLogger()->info($p . " §aThis config is up to date");
         } else {
             $this->getServer()->getLogger()->alert($p . " This config is outdated.");
             $this->getServer()->getLogger()->alert($p . " Please delete this config to recive the new config version");
             $this->getServer()->getLogger()->alert($p . " And dont forget to copy your messages");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
         }
+        $this->getServer()->getLogger()->info($p . " §aEssentialsPM Plugin got enabled successfully");
+    }
+    
+    public function onDisable() : void {
+        $this->getServer()->getLogger()->alert($p . " §cEssentialsPM Plugin got disabled successfully");
     }
 }
