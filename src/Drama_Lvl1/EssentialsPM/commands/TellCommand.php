23b4cf3358e338eeadc2da120e0287ab97ddf810
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-10-11 11:49:31 +0200

    Update TellCommand.php

--- src/Drama_Lvl1/EssentialsPM/commands/TellCommand.php
+++ src/Drama_Lvl1/EssentialsPM/commands/TellCommand.php
@@ -4,7 +4,13 @@ namespace Drama_Lvl1\EssentialsPM\commands;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use Drama_Lvl1\EssentialsPM\EssentialsPM;
 
 class TellCommand extends Command{
-
+  
+    public function __construct(EssentialsPM $plugin)
+    {
+        parent::__construct("tell", "Reply the last Private Message", "/tell <message>", ["msg"]);
+        $this->plugin = $plugin;
+    }
 }
