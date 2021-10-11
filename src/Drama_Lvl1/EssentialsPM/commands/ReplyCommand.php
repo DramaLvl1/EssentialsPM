78d67c53fe093b3cb439b4b29287f1bc01535c10
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-10-11 15:04:58 +0200

    Update ReplyCommand.php

--- src/Drama_Lvl1/EssentialsPM/commands/ReplyCommand.php
+++ src/Drama_Lvl1/EssentialsPM/commands/ReplyCommand.php
@@ -7,5 +7,15 @@ use pocketmine\command\CommandSender;
 use Drama_Lvl1\EssentialsPM\EssentialsPM;
 
 class ReplyCommand extends Command{
-
+    
+    public function __construct(EssentialsPM $plugin)
+    {
+        parent::__construct("reply", "Reply the last Private Message", "/reply <message>", ["r"]);
+        $this->plugin = $plugin;
+    }
+  
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+      
+    }
 }
