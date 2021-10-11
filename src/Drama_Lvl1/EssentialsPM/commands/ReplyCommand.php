33376516bc35ef15247e3a3ba458c44558c3931a
Drama_Lvl1 <53279807+DramaLvl1@users.noreply.github.com>
2021-10-11 11:46:57 +0200

    Create ReplyCommand.php

---
+++ src/Drama_Lvl1/EssentialsPM/commands/ReplyCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drama_Lvl1\EssentialsPM\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use Drama_Lvl1\EssentialsPM\EssentialsPM;
+
+class ReplyCommand extends Command{
+
+}
